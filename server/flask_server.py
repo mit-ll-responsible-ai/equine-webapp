285c03bb2158d3fc6ed3609287c5edab2af3fba4
Steven Jorgensen <st28850@551410-mitll.llan.ll.mit.edu>
2023-08-09 13:37:41 -0400

    added documentation to flask server code, turned off flask server debug mode

--- server/flask_server.py
+++ server/flask_server.py
@@ -3,7 +3,6 @@
 
 import os
 import io
-from pathlib import Path
 import json
 
 from flask import Flask, request, jsonify, send_file
@@ -28,9 +27,20 @@ explorer_html = ExplorerGraphiQL().html(None)
 
 # Helper Functions ###############################
 def StartServer():
-    app.run(port=5252, debug=True) #TODO remove debug
+    """
+    Helper function to quickly start the flask server on port 5252.
+    """
+    app.run(port=5252, debug=False)
 
 def clear_folder(folder_path):
+    """
+    Helper function to clear all files in the given folder on server.
+
+    Parameters
+    ----------
+    folder_path : str
+        Path of directory to be cleared.
+    """
     for filename in os.listdir(folder_path):
         os.remove(os.path.join(folder_path, filename))
 
@@ -42,11 +52,26 @@ def hello():
 
 @app.route("/graphql", methods=["GET"])
 def graphql_explorer():
-    # On GET request serve the GraphQL explorer.
+    """
+    Flask GET Route to serve GraphQL explorer UI.
+
+    Returns
+    -------
+    html, int
+        HTML of GraphQL explorer UI, along with server status code.
+    """
     return explorer_html, 200
 
 @app.route("/graphql", methods=["POST"])
 def graphql_server():
+    """
+    Flask POST Route to handle all graphQL API requests.
+
+    Returns
+    -------
+    json, int
+        Json result of GraphQL Query/Mutation along with the server status code.
+    """
     if request.content_type.startswith("multipart/form-data"):
         data = combine_multipart_data(
             json.loads(request.form.get("operations")),
@@ -66,6 +91,22 @@ def graphql_server():
 
 @app.route("/render-image/inference/<run_id>/<data_index>", methods=["GET"])
 def handle_render_inference_image(run_id, data_index):
+    """ 
+    Flask Route to send image inference as image file to the frontend.
+    
+    Parameters
+    ----------
+    run_id : int
+        ID number of the inference run that contained the image to be rendered.
+    data_index : int
+        Index in the inference run file of the image to be rendered.
+
+    Returns
+    -------
+    File
+        Selected image file.
+
+    """
     sample, _ = get_sample_from_data_index(run_id, data_index)
     return send_img_tensor_as_file(sample, f"{run_id}_{data_index}")
 
@@ -73,10 +114,42 @@ def handle_render_inference_image(run_id, data_index):
 
 @app.route("/render-image/support/<model_name>/<data_index>", methods=["GET"])
 def handle_render_support_image(model_name, data_index):
+    """ 
+    Flask Route to render model support examples on the frontend.
+    
+    Parameters
+    ----------
+    model_name : str
+        Filename of the model to get support images from.
+    data_index : int
+        Index in the support examples where the selected image resides.
+
+    Returns
+    -------
+    File
+        Selected image file
+
+    """
     support_example, _ = get_support_example_from_data_index(model_name, data_index)
     return send_img_tensor_as_file(support_example, f"{model_name}_{data_index}")
 
 def send_img_tensor_as_file(img_tensor, filename):
+    """ 
+    Flask Route to convert an image torch tensor to a PNG file.
+    
+    Parameters
+    ----------
+    image tensor : torch.Tensor
+        Tensor containing the image data.
+    filename : str
+        Name to save the converted PNG image as.
+
+    Returns
+    -------
+    File
+        Converted image file
+
+    """
     img = ToPILImage()(img_tensor.to(torch.uint8))
     img_buf = io.BytesIO()
     img.save(img_buf, format="png")
@@ -86,4 +159,18 @@ def send_img_tensor_as_file(img_tensor, filename):
 
 @app.route("/models/<model_name>", methods=["GET"])
 def handle_send_model(model_name):
+    """ 
+    Flask Route to download models from the server.
+    
+        Parameters
+        ----------
+        model_name : str
+            Filename of model to be downloaded.
+
+        Returns
+        -------
+        File
+            Selected Equine model file
+
+    """
     return send_file(os.path.join(os.getcwd(), SERVER_CONFIG.MODEL_FOLDER_PATH, model_name), download_name=model_name)
