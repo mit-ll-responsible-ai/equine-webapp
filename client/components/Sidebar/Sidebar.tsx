0c07c761838bf31a0fa7841ed92b603c82f6e4e6
Harry Li <harry.li@ll.mit.edu>
2023-10-03 14:49:26 -0400

    made sidebar responsive

--- client/components/Sidebar/Sidebar.tsx
+++ client/components/Sidebar/Sidebar.tsx
@@ -6,6 +6,7 @@ import Link from "next/link"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
+  faBars,
   faChartBar,
   faDownload,
   faGear,
@@ -22,56 +23,64 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons"
 
 export default function Sidebar() {
   return (
-    <div id={styles.sidebar}>
-      <Link href={ROUTES.LANDING}>
-        <div className={styles.navLink}>
-          Home <FontAwesomeIcon icon={faHome}/>
+    <>
+      <div id={styles.sidebar}>
+        <div id={styles["mobile-navbar"]}>
+          <FontAwesomeIcon icon={faBars}/>
         </div>
-      </Link>
 
-      <Link href={ROUTES.DASHBOARD}>
-        <div className={styles.navLink}>
-          Inference Dashboard <FontAwesomeIcon icon={faChartBar}/>
+        <div id={styles["sidebar-content"]}>
+          <Link href={ROUTES.LANDING}>
+            <div className={styles.navLink}>
+              Home <FontAwesomeIcon icon={faHome}/>
+            </div>
+          </Link>
+
+          <Link href={ROUTES.DASHBOARD}>
+            <div className={styles.navLink}>
+              Inference Dashboard <FontAwesomeIcon icon={faChartBar}/>
+            </div>
+          </Link>
+
+          <Link href={ROUTES.TRAINING}>
+            <div className={styles.navLink}>
+              Train an EQUI(NE)² Model <FontAwesomeIcon icon={faSitemap}/>
+            </div>
+          </Link>
+
+          <Link href={ROUTES.MODEL_SUMMARY_PAGE}>
+            <div className={styles.navLink}>
+              Model Summary Page <FontAwesomeIcon icon={faNewspaper}/>
+            </div>
+          </Link>
+
+          <Link href={ROUTES.DOWNLOAD_PAGE}>
+            <div className={styles.navLink}>
+              Download Page <FontAwesomeIcon icon={faDownload}/>
+            </div>
+          </Link>
+
+          <Link href={ROUTES.SETTINGS}>
+            <div className={styles.navLink}>
+              Settings Page <FontAwesomeIcon icon={faGear}/>
+            </div>
+          </Link>
+
+          <br/><br/>
+
+          <Link href={ROUTES.DEMO}>
+            <div className={styles.navLink}>
+              ScatterUQ Demo <FontAwesomeIcon icon={faMagnifyingGlassChart}/>
+            </div>
+          </Link>
+
+          <Link href="https://github.com/mit-ll-responsible-ai/equine-webapp" target="_blank">
+            <div className={styles.navLink}>
+              GitHub Repository <FontAwesomeIcon icon={faGithub}/>
+            </div>
+          </Link>
         </div>
-      </Link>
-
-      <Link href={ROUTES.TRAINING}>
-        <div className={styles.navLink}>
-          Train an EQUI(NE)² Model <FontAwesomeIcon icon={faSitemap}/>
-        </div>
-      </Link>
-
-      <Link href={ROUTES.MODEL_SUMMARY_PAGE}>
-        <div className={styles.navLink}>
-          Model Summary Page <FontAwesomeIcon icon={faNewspaper}/>
-        </div>
-      </Link>
-
-      <Link href={ROUTES.DOWNLOAD_PAGE}>
-        <div className={styles.navLink}>
-          Download Page <FontAwesomeIcon icon={faDownload}/>
-        </div>
-      </Link>
-
-      <Link href={ROUTES.SETTINGS}>
-        <div className={styles.navLink}>
-          Settings Page <FontAwesomeIcon icon={faGear}/>
-        </div>
-      </Link>
-
-      <br/><br/>
-
-      <Link href={ROUTES.DEMO}>
-        <div className={styles.navLink}>
-          ScatterUQ Demo <FontAwesomeIcon icon={faMagnifyingGlassChart}/>
-        </div>
-      </Link>
-
-      <Link href="https://github.com/mit-ll-responsible-ai/equine-webapp">
-        <div className={styles.navLink}>
-          GitHub Repository <FontAwesomeIcon icon={faGithub}/>
-        </div>
-      </Link>
-    </div>
+      </div>
+    </>
   )
 }
